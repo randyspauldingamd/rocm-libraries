1cb55f691700258261603c42468f8fd98ad1cb09
Sandhu, Prabhjot <Prabhjot.Sandhu@amd.com>
2024-07-17 12:58:10 -0400

    Added more expansive emulator test suite
    * added new filters
    * skip some tests
    * run rrperf for only f8, f6, f4
    
    [ROCm/rocRoller commit: 31c8db5d29a455239ab7d6573c6067d411ce5bbb]

--- shared/rocroller/test/runtests.sh
+++ shared/rocroller/test/runtests.sh
@@ -8,20 +8,15 @@ RRTESTS=$(realpath build/rocRollerTests)
 RRPERF=$(realpath scripts/rrperf)
 
 # Tests for gfx950
-TESTS=("*GPU_MatrixMultiplyMacroTileF8_16x16x32_NN*"
+F8TESTS=("*GPU_MatrixMultiplyMacroTileF8_16x16x32_NN*"
 "*GPU_MatrixMultiplyMacroTileF8_32x32x16_NN*"
 "*GPU_MatrixMultiplyMacroTileF8_16x16x32_TN*"
 "*GPU_MatrixMultiplyMacroTileF8_32x32x64_TN*"
 "*GPU_MatrixMultiplyMacroTileF8_16x16x128_TN*"
-"*GPU_MatrixMultiplyMacroTileF6_16x16x128_TN*"
-"*GPU_MatrixMultiplyMacroTileF6_32x32x64_TN*"
-"*GPU_MatrixMultiplyMacroTileFP4_16x16x128_TN*"
-"*GPU_MatrixMultiplyMacroTileFP4_32x32x64_TN*"
 "*GPU_MatrixMultiplyABF8_16x16x32*"
 "*GPU_MatrixMultiplyABF8_32x32x16*"
 "*GPU_MatrixMultiplyABF8_16x16x128*"
 "*GPU_MatrixMultiplyABF8_32x32x64*"
-"*ScaledMatrixMultiplyTestGPU*"
 "*GPU_BasicGEMMFP8_16x16x32_NT*"
 "*GPU_BasicGEMMFP8_16x16x128_NT"
 "*GPU_BasicGEMMBF8_16x16x128_NT"
@@ -31,27 +26,115 @@ TESTS=("*GPU_MatrixMultiplyMacroTileF8_16x16x32_NN*"
 "*GPU_BasicGEMMBF8_16x16x128_TN"
 "*GPU_BasicGEMMFP8_32x32x64_TN"
 "*GPU_BasicGEMMBF8_32x32x64_TN"
+)
+
+F6TESTS=("*GPU_MatrixMultiplyMacroTileF6_16x16x128_TN*"
+"*GPU_MatrixMultiplyMacroTileF6_32x32x64_TN*"
 "*GPU_BasicGEMMFP6_16x16x128_TN"
 "*GPU_BasicGEMMFP6_32x32x64_TN"
 "*GPU_BasicGEMMBF6_16x16x128_TN"
 "*GPU_BasicGEMMBF6_32x32x64_TN"
+)
+
+F4TESTS=("*GPU_MatrixMultiplyMacroTileFP4_16x16x128_TN*"
+"*GPU_MatrixMultiplyMacroTileFP4_32x32x64_TN*"
 "*GPU_BasicGEMMFP4_16x16x128_TN"
 "*GPU_BasicGEMMFP4_32x32x64_TN"
 )
 
-RRPERF_TESTS=("f8gemm_16x16x128_f8f6f4"
+MISCTESTS=("*ScaledMatrixMultiplyTestGPU*"
+)
+
+SKIPTESTS=("*BasicGEMMFP16Prefetch3*"
+"*VectorAddBenchmark*"
+)
+
+RRPERF_F8TESTS=("f8gemm_16x16x128_f8f6f4"
 "f8gemm_32x32x64_f8f6f4"
-"f6gemm_16x16x128_f8f6f4"
+)
+
+RRPERF_F6TESTS=("f6gemm_16x16x128_f8f6f4"
 "f6gemm_32x32x64_f8f6f4"
-"f4gemm_16x16x128_f8f6f4"
+)
+
+RRPERF_F4TESTS=("f4gemm_16x16x128_f8f6f4"
 "f4gemm_32x32x64_f8f6f4"
 )
 
-# Loop through each test and execute it
-for testName in "${TESTS[@]}"; do
-    $RRTESTS --gtest_filter="$testName"
+
+
+
+
+suite="small"
+while getopts "t:" opt; do
+    case "${opt}" in
+    t) suite="${OPTARG,,}";;
+    [?]) echo >&2 "Usage: $0 [-t option]
+             option: {f8 | f6 | f4 | small | full}"
+         exit 1;;
+    esac
 done
-# Run rrperf tests
-for testName in "${RRPERF_TESTS[@]}"; do
-    $RRPERF run --suite "$testName"
-done
+
+
+if [ "$suite" = "full" ]; then
+    skip=${SKIPTESTS[*]}
+    $RRTESTS --gtest_filter="-${skip// /:}"
+
+    # Run rrperf tests
+    for testName in "${RRPERF_F8TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+    for testName in "${RRPERF_F4TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+    for testName in "${RRPERF_F6TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+elif [ "$suite" = "f8" ]; then
+    for testName in "${F8TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${RRPERF_F8TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+elif [ "$suite" = "f6" ]; then
+    for testName in "${F6TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${RRPERF_F6TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+elif [ "$suite" = "f4" ]; then
+    for testName in "${F4TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${RRPERF_F4TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+else
+    # Loop through each test and execute it
+    for testName in "${F8TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${F6TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${F4TESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+    for testName in "${MISCTESTS[@]}"; do
+        $RRTESTS --gtest_filter="$testName"
+    done
+
+    # Run rrperf tests
+    for testName in "${RRPERF_F8TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+    for testName in "${RRPERF_F4TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+    for testName in "${RRPERF_F6TESTS[@]}"; do
+        $RRPERF run --suite "$testName"
+    done
+fi
+
