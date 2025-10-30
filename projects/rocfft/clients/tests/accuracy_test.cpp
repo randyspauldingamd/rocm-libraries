622967f785cff17e8b4fd87c5cfa4579fd2bbcf0
Steve Leung <Steve.Leung@amd.com>
2025-10-30 09:12:39 -0600

    rocfft: clarify callback documentation for arrays and same-ness (#2328)
    ## Motivation
    
    Improve documentation and tests for callback functions.
    
    ## Technical Details
    
    Callback functions are meant to be specified to
    `rocfft_plan_description_set_load_callback` or
    `rocfft_plan_description_set_store_callback` as arrays of function
    pointers. The intent is that for an N-device transform, N function
    pointers must be specified, as the pointers point to per-device
    functions.
    
    Updated the tests to actually collect N function pointers into an array,
    for an N-device transform.
    
    Updated the documentation to make this clear.
    
    Also clarified that the functions supplied in an array must all behave
    the same, as rocFFT cannot guarantee exactly which data is passed to
    which function in an array. Updated the tests to not attempt to depend
    on this non-existent guarantee.
    
    ## Test Plan
    
    Existing callback tests have been improved.
    
    ## Test Result
    
    Tests pass.
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- projects/rocfft/clients/tests/accuracy_test.cpp
+++ projects/rocfft/clients/tests/accuracy_test.cpp
@@ -30,14 +30,7 @@ __host__ __device__ Tdata load_callback(Tdata* input, size_t offset, void* cbdat
 {
     auto testdata = static_cast<const callback_test_data*>(cbdata);
     // multiply each element by scalar
-    if(input == testdata->base)
-        return input[offset] * testdata->scalar;
-    // wrong base address passed, return something obviously wrong
-    else
-    {
-        // wrong base address passed, return something obviously wrong
-        return input[0];
-    }
+    return input[offset] * testdata->scalar;
 }
 
 __device__ auto load_callback_dev_half           = load_callback<rocfft_fp16>;
@@ -55,14 +48,7 @@ __host__ __device__ Tdata
 {
     auto testdata = static_cast<const callback_test_data*>(cbdata);
     // subtract each element by scalar
-    if(input == testdata->base)
-        return input[offset] - testdata->scalar;
-    // wrong base address passed, return something obviously wrong
-    else
-    {
-        // wrong base address passed, return something obviously wrong
-        return input[0];
-    }
+    return input[offset] - testdata->scalar;
 }
 
 __device__ auto load_callback_round_trip_inverse_dev_half
@@ -212,11 +198,7 @@ __host__ __device__ static void
 {
     auto testdata = static_cast<callback_test_data*>(cbdata);
     // add scalar to each element
-    if(output == testdata->base)
-    {
-        output[offset] = element + testdata->scalar;
-    }
-    // otherwise, wrong base address passed, just don't write
+    output[offset] = element + testdata->scalar;
 }
 __device__ auto store_callback_dev_half           = store_callback<rocfft_fp16>;
 __device__ auto store_callback_dev_complex_half   = store_callback<rocfft_complex<rocfft_fp16>>;
@@ -231,11 +213,7 @@ __host__ __device__ static void store_callback_round_trip_inverse(
 {
     auto testdata = static_cast<callback_test_data*>(cbdata);
     // divide each element by scalar
-    if(output == testdata->base)
-    {
-        output[offset] = element / testdata->scalar;
-    }
-    // otherwise, wrong base address passed, just don't write
+    output[offset] = element / testdata->scalar;
 }
 __device__ auto store_callback_round_trip_inverse_dev_half
     = store_callback_round_trip_inverse<rocfft_fp16>;
@@ -386,7 +364,6 @@ void apply_store_callback(const fft_params& params, std::vector<hostbuf>& output
 
     callback_test_data cbdata;
     cbdata.scalar = params.store_cb_scalar;
-    cbdata.base   = output.front().data();
 
     switch(params.otype)
     {
@@ -506,7 +483,6 @@ void apply_load_callback(const fft_params& params, std::vector<hostbuf>& input)
 
     callback_test_data cbdata;
     cbdata.scalar = params.load_cb_scalar;
-    cbdata.base   = input.front().data();
 
     switch(params.itype)
     {
