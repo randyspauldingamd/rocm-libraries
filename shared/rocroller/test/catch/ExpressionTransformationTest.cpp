426358dc8b59327462e72e110975af915a2cf03a
Caio S. Rohwedder <Caio.SalvadorRohwedder@amd.com>
2025-10-31 00:07:58 -0600

    [rocRoller] Add more simplify passes to FastArithmetic pipeline (#2336)
    ## Motivation
    
    The `simplify` expression transformation is currently called twice in
    the `FastArithmetic` pipeline, once explicitly and once inside
    `splitBitfieldCombine`. This PR adds another simplify call at the end of
    the pipeline, as it further simplifies expressions present in GEMM
    kernels.
    
    It seems that the generated code does not change because the
    `FastArithmetic` is called twice by the code generator, but it may
    benefit other places in the code that call `FastArithmetic` only once.
    
    ## Technical Details
    
    - Adds a simplify call to `FastArithmetic` 
    - Adds `applyTransforms` and `getTransforms` functions to
    `FastArithmetic` to enable testing with different pipelines
    
    ## Test Plan
    
    - Adds test "FastArithmetic pipeline properly simplifies expressions"
    with an example that benefits from the extra simplify

--- shared/rocroller/test/catch/ExpressionTransformationTest.cpp
+++ shared/rocroller/test/catch/ExpressionTransformationTest.cpp
@@ -430,6 +430,64 @@ TEST_CASE("FastArithmetic includes translate time evaluation",
     CHECK_THAT(fastArith(c * zero), IdenticalTo(literal(0.f)));
 }
 
+TEST_CASE("FastArithmetic pipeline properly simplifies expressions",
+          "[expression][expression-transformation]")
+{
+    using namespace rocRoller;
+    using Expression::literal;
+    auto context = TestContext::ForDefaultTarget();
+
+    Expression::FastArithmetic fastArith(context.get());
+    auto                       transforms = fastArith.getTransforms();
+
+    auto isSimplify = [](const Expression::ExpressionTransformType& transformFunction) {
+        using ExprTransformFuncPtrType = Expression::ExpressionPtr (*)(Expression::ExpressionPtr);
+        const auto* funcPtr            = transformFunction.target<ExprTransformFuncPtrType>();
+        return funcPtr && *funcPtr == Expression::simplify;
+    };
+
+    // Create a version with extra simplifies after each non-simplify transform
+    std::vector<Expression::ExpressionTransformType> transformsExtraSimplify;
+    for(const auto& transform : transforms)
+    {
+        transformsExtraSimplify.push_back(transform);
+        if(!isSimplify(transform))
+            transformsExtraSimplify.push_back(Expression::simplify);
+    }
+
+    // Create a version with only one simplify
+    std::vector<Expression::ExpressionTransformType> transformsOneSimplify;
+    bool                                             hasSimplify = false;
+    for(const auto& transform : transforms)
+    {
+        if(isSimplify(transform))
+        {
+            if(hasSimplify)
+                continue;
+
+            hasSimplify = true;
+        }
+        transformsOneSimplify.push_back(transform);
+    }
+
+    auto tag83 = Expression::dataFlowTag(83, Register::Type::Vector, DataType::UInt32);
+    auto expr  = (tag83 % literal(4)) * literal(32);
+    expr       = (tag83 / literal(4)) * literal(128u) + expr;
+    expr       = expr * literal(4u);
+    expr       = expr / literal(8u);
+    expr       = expr + literal(0u);
+    expr       = std::make_shared<Expression::Expression>(Expression::ToScalar{expr});
+    expr       = Expression::convert(DataType::UInt32, expr);
+
+    // One simplify is not enough
+    CHECK(!Expression::identical(fastArith.applyTransforms(expr, transforms),
+                                 fastArith.applyTransforms(expr, transformsOneSimplify)));
+
+    // There are enough simplifies
+    CHECK_THAT(fastArith.applyTransforms(expr, transforms),
+               IdenticalTo(fastArith.applyTransforms(expr, transformsExtraSimplify)));
+}
+
 TEST_CASE("ConvertPropagation", "[expression][expression-transformation]")
 {
     using namespace rocRoller;
