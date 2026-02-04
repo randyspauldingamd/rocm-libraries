6086ba315c600fce2de6c9318737dbf0f5f29a64
CMiservaAMD <cmiserva@amd.com>
2026-02-04 07:38:16 -0700

    [hipDNN] SDK versioning cmake tweaks. (#4311)
    Address some review comments from SDK versioning PR: convert some macros to functions and consolidate some duplicated code,

--- projects/hipdnn/cmake/VersionUtils.cmake
+++ projects/hipdnn/cmake/VersionUtils.cmake
@@ -1,9 +1,9 @@
 # Copyright © Advanced Micro Devices, Inc., or its affiliates.
 # SPDX-License-Identifier:  MIT
 
-# Macro to setup versioning for a component
+# Function to setup versioning for a component
 # Reads version.json, gets git hash, sets version variables, and calls project()
-macro(hipdnn_setup_version COMPONENT_NAME)
+function(hipdnn_setup_version COMPONENT_NAME)
     string(TOUPPER ${COMPONENT_NAME} COMPONENT_NAME_UPPER)
 
     # Read version from version.json
@@ -35,31 +35,44 @@ macro(hipdnn_setup_version COMPONENT_NAME)
     project(${COMPONENT_NAME} VERSION ${${COMPONENT_NAME_UPPER}_VERSION} LANGUAGES CXX)
 
     message(STATUS "${COMPONENT_NAME} version: ${${COMPONENT_NAME_UPPER}_VERSION_STRING}")
-endmacro()
+
+    # Propagate version variables to parent scope
+    set(${COMPONENT_NAME_UPPER}_VERSION_MAJOR ${${COMPONENT_NAME_UPPER}_VERSION_MAJOR} PARENT_SCOPE)
+    set(${COMPONENT_NAME_UPPER}_VERSION_MINOR ${${COMPONENT_NAME_UPPER}_VERSION_MINOR} PARENT_SCOPE)
+    set(${COMPONENT_NAME_UPPER}_VERSION_PATCH ${${COMPONENT_NAME_UPPER}_VERSION_PATCH} PARENT_SCOPE)
+    set(${COMPONENT_NAME_UPPER}_VERSION_TWEAK ${${COMPONENT_NAME_UPPER}_VERSION_TWEAK} PARENT_SCOPE)
+    set(${COMPONENT_NAME_UPPER}_VERSION_STRING ${${COMPONENT_NAME_UPPER}_VERSION_STRING} PARENT_SCOPE)
+endfunction()
 
 # Function to generate the version header
-macro(hipdnn_generate_version_header COMPONENT_NAME)
-    set(_include_dir "${CMAKE_CURRENT_SOURCE_DIR}/include")
+function(hipdnn_generate_version_header COMPONENT_NAME)
     configure_file(
-        "${_include_dir}/${COMPONENT_NAME}/version.h.in"
+        "${CMAKE_CURRENT_SOURCE_DIR}/version.h.in"
         "${CMAKE_CURRENT_BINARY_DIR}/include/${COMPONENT_NAME}/version.h"
         @ONLY
     )
-endmacro()
+
+    # Add generated include directory for build interface
+    target_include_directories(
+        ${COMPONENT_NAME} INTERFACE $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
+    )
+endfunction()
 
 # Capture the directory where this file resides
 set(HIPDNN_CMAKE_UTILS_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-# Macro to read data_sdk version for minimum requirement
-macro(hipdnn_get_data_sdk_version OUTPUT_VAR)
+# Function to read data_sdk version for minimum requirement
+function(hipdnn_get_data_sdk_version OUTPUT_VAR)
     # Determine path to data_sdk/version.json relative to this file location
     # This makes it resilient to where the macro is called from
     set(_data_sdk_version_file "${HIPDNN_CMAKE_UTILS_DIR}/../data_sdk/version.json")
 
     if(EXISTS "${_data_sdk_version_file}")
         file(READ "${_data_sdk_version_file}" _data_sdk_version_json)
-        string(JSON ${OUTPUT_VAR} GET ${_data_sdk_version_json} "hipdnn_data_sdk_version")
+        string(JSON _version_value GET ${_data_sdk_version_json} "hipdnn_data_sdk_version")
+        # Propagate OUTPUT_VAR to parent scope
+        set(${OUTPUT_VAR} ${_version_value} PARENT_SCOPE)
     else()
         message(FATAL_ERROR "Could not find data_sdk version file at ${_data_sdk_version_file}")
     endif()
-endmacro()
+endfunction()
