139bb631292489f29ea4bbcb08102870c321f7ec
Stacey Lai <stacelai@amd.com>
2026-03-10 09:52:33 +0000

    Fix DVT tail loop validation failure on gfx12 by adding missing bpe multiply
    The GLOBAL_OFFSET macro was refactored to move the BPE multiplication out of
    the macro and into each call site. However, the globalReadGuardK function's
    call to GLOBAL_OFFSET for computing the max valid address offset in DVT tail
    loops was not updated to include the external bpe multiply.

--- projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -9162,6 +9162,7 @@ class KernelWriterAssembly(KernelWriter):
       module.add(VSubU32(dst=vgpr(tmp2), src0=vgpr(tmp2), src1=1, comment="GLTr%s: unroll idx - 1"%(tc)))
       bfArgs = (maxGroVgpr, maxGroVgpr, tmp2, tmp)
       module.add(MacroInstruction(name="GLOBAL_OFFSET_%s"%tc, args=bfArgs))
+      module.add(vectorMultiplyBpe(maxGroVgpr, maxGroVgpr, tP["bpeGR"]))
 
       module.addSpaceLine()
 
