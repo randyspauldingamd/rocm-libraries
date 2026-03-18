fb7279c9ea436b17a03ab2b2ee02b1071750fbac
Stacey Lai <stacey.lai@amd.com>
2026-03-18 06:36:27 +0000

    Fix activation args type mismatch causing segfault in debug build
    prob.act0/act1 (float) were pushed into ConstantVariant without casting to
    the compute type. When compute type is Int32 (i8->i32 GEMM), subsequent
    std::get_if<int32_t> on a float-holding variant returned nullptr, causing
    SIGSEGV on dereference. Release builds masked this UB via -O3 optimizations.
    
    Affects: ./build/release/clients/hipblaslt-test --gtest_filter=*gemm_i8_dst_i32_94x*

--- projects/hipblaslt/library/src/amd_detail/rocblaslt/src/tensile_host.cpp
+++ projects/hipblaslt/library/src/amd_detail/rocblaslt/src/tensile_host.cpp
@@ -2042,8 +2042,8 @@ namespace
         // push 2 activation arguments
         std::visit(
             [&inputs, &prob](auto val) {
-                inputs.activationArgs.push_back(prob.act0);
-                inputs.activationArgs.push_back(prob.act1);
+                inputs.activationArgs.push_back(static_cast<decltype(val)>(prob.act0));
+                inputs.activationArgs.push_back(static_cast<decltype(val)>(prob.act1));
                 if(prob.k)
                     inputs.alpha = *(decltype(val)*)(prob.alpha);
                 else
