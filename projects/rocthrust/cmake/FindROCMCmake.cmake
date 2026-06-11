7f54c6ade4d4e9d8d1a0d10f99a4ab312bf8dc6f
Saiyang Zhang <41819448+Saiyang-Zhang@users.noreply.github.com>
2026-06-11 22:39:06 +0200

    Replace download project with fetchcontent (#6573)
    ## Motivation
    
    The `download_project` funtion used to fetch libraries is outdated, we
    should use modern CMake `FetchContent`.
    
    ## Technical Details
    
    Removed `DownloadProject.cmake`, `DownloadProject.CMakeLists.cmake.in`
    which defines the `download_project` function.
    
    Add `fetc_content_isolated` which uses `FetchContent` to replace
    `download_project`, avoiding parent project flags penetrating into
    sub-libraries.
    
    Removed `LIST_SEPARATOR` replacing `;` with `|`, because cmake can parse
    it correctly. with `CMAKE_ARGS "-DGPU_TARGETS=${GPU_TARGETS}"`.
    
    Removed `INSTALL_DIR` relevant because `FetchContent_MakeAvailable` does
    the compilation and doesn't need location specification.
    
    Removed `LOG_*` arguments because they are skipped by
    `FetchContent_MakeAvailable`.
    
    Add `target_compile_options(generate_resource_spec PRIVATE
    -Wno-unused-command-line-argument)` because test fail with `-Werror` for
    unused `--offload-compress`, which is not necessary.
    
    The method in
    https://github.com/ROCm/rocm-libraries/commit/efd316edd19d13bd7eaf91160c2960055ed6fd42
    worked but not in
    https://github.com/ROCm/rocm-libraries/commit/5f26be8f12ccc1e972917df2da00bc7fcc06d139.
    An alternative method is used instead.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- projects/rocthrust/cmake/FindROCMCmake.cmake
+++ projects/rocthrust/cmake/FindROCMCmake.cmake
@@ -1,5 +1,5 @@
 # ########################################################################
-# Copyright 2021-2025 Advanced Micro Devices, Inc.
+# Copyright 2021-2026 Advanced Micro Devices, Inc.
 # ########################################################################
 
 if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
@@ -14,14 +14,21 @@ if(NOT ROCmCMakeBuildTools_FOUND)
   else()
     set(SOURCE_SUBDIR_ARG)
   endif()
-  include(FetchContent)
-  FetchContent_Declare(
+  include(cmake/FetchContentIsolated.cmake)
+  fetch_content_isolated(
     rocm-cmake
     GIT_REPOSITORY https://github.com/ROCm/rocm-cmake.git
     GIT_TAG        rocm-6.4.4
     ${SOURCE_SUBDIR_ARG}
   )
-  FetchContent_MakeAvailable(rocm-cmake)
+  execute_process(
+    WORKING_DIRECTORY ${rocm-cmake_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} ${rocm-cmake_SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=.
+  )
+  execute_process(
+    WORKING_DIRECTORY ${rocm-cmake_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} --build ${rocm-cmake_SOURCE_DIR} --target install
+  )
   find_package(ROCmCMakeBuildTools CONFIG REQUIRED NO_DEFAULT_PATH PATHS "${rocm-cmake_SOURCE_DIR}")
 else()
   find_package(ROCmCMakeBuildTools 0.7.3 CONFIG REQUIRED PATHS "${ROCM_ROOT}")
